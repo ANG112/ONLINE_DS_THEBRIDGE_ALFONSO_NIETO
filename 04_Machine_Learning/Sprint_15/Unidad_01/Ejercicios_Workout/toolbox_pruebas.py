c261ed7f9621885847d128dd8de5e18378e9f6d0
Alfonso Nieto García <43706214+ANG112@users.noreply.github.com>
2024-05-20 17:29:12 +0200

    workout_1 sp_15 _und_1

---
+++ 04_Machine_Learning/Sprint_15/Unidad_01/Ejercicios_Workout/toolbox_pruebas.py
@@ -0,0 +1,49 @@
+''' 
+
+'''
+import numpy as np
+import os
+
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+
+%matplotlib inline
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+
+
+
+# Hallar la gráfica del score de inercia
+
+kmeans_per_k = [KMeans(n_clusters=k, random_state= 42, n_init = 25).fit(X)
+                for k in range(1, 10)]
+inertias = [model.inertia_ for model in kmeans_per_k]
+
+plt.figure(figsize=(8, 3.5))
+plt.plot(range(1, 10), inertias, "bo-")
+plt.xlabel("$k$", fontsize=14)
+plt.ylabel("Inertia", fontsize=14)
+plt.annotate('Elbow/Codo',
+             xy=(4, inertias[3]),
+             xytext=(0.55, 0.55),
+             textcoords='figure fraction',
+             fontsize=16,
+             arrowprops=dict(facecolor='black', shrink=0.1)
+            )
+plt.axis([1, 8.5, 0, 1300])
+plt.show()
+
+
+# Hallar la gráfica del score de siluetas
+silhouette_scores = [silhouette_score(X, model.labels_)
+                     for model in kmeans_per_k[1:]]
+
+plt.figure(figsize=(8, 3))
+plt.plot(range(2, 10), silhouette_scores, "bo-")
+plt.xlabel("$k$", fontsize=14)
+plt.ylabel("Silhouette score", fontsize=14)
+plt.axis([1.8, 8.5, 0.55, 0.7])
+plt.show()
